985de43ea9b51390d2c6ba184fb41e9a639c2151
AbnocChinwads <rnemo@hotmail.co.uk>
2026-02-19 22:59:36 +0000

    initial commit. Set up basic files and folder structure

---
+++ app.js
@@ -0,0 +1,37 @@
+import "dotenv/config";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const express = require('express');
+const app = express();
+const port = process.env.APP_PORT;
+
+// Initiliase Database connection
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_URL,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+try {
+  db.connect();
+  console.log("Database connected successfully.");
+} catch (err) {
+  console.error("Database connection error:", err.stack);
+  process.exit(1); // Exit the application if the database connection fails
+}
+
+app.use(bodyParser.urlencoded({ extended: true })); // Allows us to pass webpage information to the server
+app.use(express.static("public")); // Allows use of static files with expressjs
+
+app.get('/', (req, res) => {
+  //res.sendFile(__dirname + '/public/index.html');
+  res.send('Welcome to the Resource Management Game!');
+  res.end();
+});
+
+app.listen(port, () => {
+  console.log(`Resource management game app listening at http://localhost:${port}`);
+});
