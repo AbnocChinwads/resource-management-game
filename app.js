837d899426d8956ca99bb19da8c2cb6dbb6c4886
AbnocChinwads <rnemo@hotmail.co.uk>
2026-03-04 23:47:18 +0000

    Restructured database to support scalable game logic. Refactored frontend to use server side grow times. Updated harvest logic to move timing to db from js. Removed hardcoded resource and timing.

--- app.js
+++ app.js
@@ -26,34 +26,44 @@ try {
 app.use(bodyParser.urlencoded({ extended: true })); // Allows us to pass webpage information to the server
 app.use(express.static("public")); // Allows use of static files with expressjs
 
-app.get('/', async (req, res) => {
-    const cropsRes = await db.query("SELECT * FROM crops ORDER BY id ASC");
-    const bankedRes = await db.query("SELECT * FROM resources ORDER BY id ASC");
-    /*cropsRes.rows.forEach(crop => {
-      if (Date.now() >= new Date(crop.planted_at).getTime() + 10000) {
-        crop.isReady = true;
-      } else {
-        crop.isReady = false;
-      }
-    });*/
-    res.render('index.ejs', { crops: cropsRes.rows, resources: bankedRes.rows });
+app.get("/", async (req, res) => {
+  const cropsRes = await db.query(`
+  SELECT crops.*, crop_types.grow_time_seconds
+  FROM crops
+  JOIN crop_types ON crops.crop_type_id = crop_types.id
+  ORDER BY crops.id ASC
+  `);
+  const bankedRes = await db.query("SELECT * FROM resources ORDER BY id ASC");
+  res.render("index.ejs", { crops: cropsRes.rows, resources: bankedRes.rows });
 });
 
-app.post('/plant', async (req, res) => {
-    await db.query("INSERT INTO crops (planted_at) VALUES (NOW())");
-    res.redirect('/');
+app.post("/plant", async (req, res) => {
+  const cropType = req.body.cropTypeId
+  await db.query(
+    "INSERT INTO crops (crop_type_id, planted_at, harvested) VALUES ($1, NOW(), FALSE)",[cropType]
+  );
+  res.redirect("/");
 });
 
-app.post('/harvest', async (req, res) => {
-    const cropId = req.body.cropId;
-    const result = await db.query("UPDATE crops SET harvested = TRUE WHERE id = $1 AND harvested = FALSE AND NOW() >= planted_at + INTERVAL '10 seconds' RETURNING id", [cropId]);
-    if (result.rows.length > 0) {
-        await db.query("UPDATE resources SET amount = amount + 1 WHERE resource_type = 'wheat'");
-    }
-    
-    res.redirect('/');
+app.post("/harvest", async (req, res) => {
+  const cropId = req.body.cropId;
+  const result = await db.query(`
+    UPDATE crops SET harvested = TRUE 
+    FROM crop_types WHERE crops.id = $1 AND crops.harvested = FALSE AND crops.crop_type_id = crop_types.id 
+    AND NOW() >= crops.planted_at + crop_types.grow_time_seconds * INTERVAL '1 second' RETURNING crops.id`,
+    [cropId]
+  );
+  if (result.rows.length > 0) {
+    await db.query(
+      "UPDATE resources SET amount = amount + 1 WHERE resource_type = 'wheat'",
+    );
+  }
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
-  console.log(`Resource management game app listening at http://localhost:${port}`);
+  console.log(
+    `Resource management game app listening at http://localhost:${port}`,
+  );
 });
